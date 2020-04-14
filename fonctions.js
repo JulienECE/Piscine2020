15549b8a6330a45533dc7988f7a7f17e586f0866
carogane <valentin.martins78800@gmail.com>
2020-04-14 18:05:19 +0200

    Revert "bdd"
    This reverts commit 1310aa20b11247a303786718126c3100e145b188.

---
+++ fonctions.js
@@ -0,0 +1,16 @@
+    function salutation() {
+      var pseudo = prompt ("Entrez votre Pseudo, svp");
+      if (pseudo == null || pseudo == ""){
+        alert("Pseudo incorrect, veuillez réessayer");
+      }
+      else{
+        var mdp = prompt ("Entrez votre mdp, svp");
+        if (mdp == null || mdp == ""){ /*Faire une boucle + bdd pour tester que le mdp est compatible*/
+        alert("Mot de passe incorrect, veuillez réessayer");
+        }
+        else{
+          location.href="file:///C:/Users/julie/Desktop/Piscine2020-master/Piscine2020/Home.html"
+        }
+      }
+    }
+  
