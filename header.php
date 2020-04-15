3dae5ae663f08d2be2eaeb72e46553450971c6d8
carogane <valentin.martins78800@gmail.com>
2020-04-15 23:38:22 +0200

    function vendre

--- header.php
+++ header.php
@@ -51,7 +51,7 @@
             </li>
             <li><span class="glyphicon glyphicon-shopping-cart" style="color: white;"></span></li>
 	          <li><a href="Panier.php">Panier</a></li>
-	          <li><a href="signin.php"><span class="glyphicon glyphicon-log-in"></span> Login</a></li>
+	          <li><span id="Log" class="glyphicon glyphicon-log-in" onclick="salutation()"></span><p id="Log" onclick="salutation()"> Login</p></li>
             <li><a href="Register.php">Register</a></li>
 	    </ul>
     </div>
