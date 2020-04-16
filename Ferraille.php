c90ec5257da41a3f0acdd207a66979345dbc8103
JulienECE <47591531+JulienECE@users.noreply.github.com>
2020-04-16 21:23:51 +0200

    Update Ferraille.php

--- Ferraille.php
+++ Ferraille.php
@@ -9,30 +9,27 @@
   <div class="row content">  
     <div class="col-sm-12"> 
     	<h4 >Ferraille ou trésor</h4>
-    	<div class="row">
     		<br>
-    		<div class="col-md-4">
     			<div class="thumbnail">
 					<p class="caption">Prix : 300€ (Vente par négociation)</p>
 					<a href="#"><img src="img1.jpg"></a>
  					<input type="submit" name="buttonNégo" value="Négocier">
-				</div>
-			</div>
-	    		<div class="col-md-4">
+				</div>	    		
 	    			<div class="thumbnail">
 						<p class="caption">Prix actuel : 200€ (Vente par enchère)</p>
 						<a href="#"><img src="img1.jpg"></a>
             			<input type="submit" name="buttonEnchere" value="Enchérir">
 					</div>
-				</div>
-	    		<div class="col-md-4">
 	    			<div class="thumbnail">
 						<p class="caption">Prix : 200€ (Vente directe)</p>
 						<a href="#"><img src="img1.jpg"></a>
             			<input type="submit" name="buttonAcheter" value="Acheter">
 					</div>
-				</div>
-			</div>
+	    			<div class="thumbnail">
+						<p class="caption">Prix : 200€ (Vente directe)</p>
+						<a href="#"><img src="img1.jpg"></a>
+            			<input type="submit" name="buttonAcheter" value="Acheter">
+					</div>
 		</div>
 		</div>
 		</div>
