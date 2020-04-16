5e0611df99d37c11d987bb5932023afabafea0d5
JulienECE <47591531+JulienECE@users.noreply.github.com>
2020-04-16 11:11:19 +0200

    Update Vendre.php

--- Vendre.php
+++ Vendre.php
@@ -2,22 +2,20 @@
 
 <div class="container-fluid text-center">    
   <div class="row content">
-    <!-- Colonne de gauche-->
+  	<!-- Colonne de gauche-->
     <div class="col-sm-1 sidenav"></div>
 
-<div class="col-sm-8"> 
-  <h3>Formulaire d'ajout des items à vendre</h3>
-    <form action="additem.php" method="post" class="formulaire_val">
-       <p><input class="inputbasic" type="text" name="titre" placeholder="Titre de l'objet"/></p>
-       <p><textarea class="inputbasic" placeholder="Description"/></textarea></p>
-       <p><input class="inputbasic" type="file" value="photo"></p>
-       <p><input class="inputbasic" type="file"></p>
-       <p align="left"><input type="text" name="prix" placeholder="Prix"/></p>
-       <p align="left"><input type="text" name="pseudo" placeholder="Votre pseudo"/></p>
-       <p align="left"><input type="submit" name="boutonValider" value="Valider" ></p>
-    </form>
-</div>
+    <div class="col-sm-8"> 
+		<form action="additem.php" method="post" class="formulaire_val">
+		   <p><input class="inputbasic" type="text" name="titre" placeholder="Titre de l'objet"/></p>
+		   <p><textarea class="inputbasic" name="description" placeholder="Description"/></textarea></p>
+		   <p><input class="inputbasic" type="file"></p>
+		   <p><input class="inputbasic" type="file"></p>
+		   <p align="left"><input type="text" name="prix" placeholder="Prix"/></p>
+		   <p align="left"><input type="text" name="pseudo" placeholder="Pseudo"/></p>
+		   <p align="left"><input type="submit" name="valider" /></p>
+		</form>
+	</div>
 
-
-<?php require 'ColonneDroite.php'; ?>
-<?php require 'footer.php'; ?>
+	<?php require 'ColonneDroite.php'; ?>
+	<?php require 'footer.php'; ?>
