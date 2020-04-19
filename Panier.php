35760132691e6e49a99233ec9d8e5d9f5709c7a5
JulienECE <47591531+JulienECE@users.noreply.github.com>
2020-04-19 19:29:31 +0200

    Update Panier.php

--- Panier.php
+++ Panier.php
@@ -2,47 +2,49 @@
 <!-- Colonne du milieu-->
 <div class="container-fluid p-0">
     	<h3>Votre Panier<br><br></h3>
-    	<div id="panier" class="row">
+    	<div class="container-fluid">
+	    	<div id="panier" class="row">
+						<tr>
+							<div class="col-sm-1"><td>ID</td></div>
+							<div class="col-sm-3"><td>Nom de l'article</td></div>
+							<div class="col-sm-6"><td>Description</td></div>
+							<div class="col-sm-2"><td>Prix</td></div>
+						</tr>
+						<tr>
+							<div class="col-sm-1"><td>1</td></div>
+							<div class="col-sm-3"><td>...</td></div>
+							<div class="col-sm-6"><td>....</td></div>
+							<div class="col-sm-2"><td>....€</td></div>
+						</tr>
+						<tr>
+							<div class="col-sm-1"><td>2</td></div>
+							<div class="col-sm-3"><td>...</td></div>
+							<div class="col-sm-6"><td>....</td></div>
+							<div class="col-sm-2"><td>....€</td></div>
+						</tr>
+						<tr>
+							<div class="col-sm-1"><td>3</td></div>
+							<div class="col-sm-3"><td>...</td></div>
+							<div class="col-sm-6"><td>....</td></div>
+							<div class="col-sm-2"><td>....€</td></div>
+						</tr>
+			</div>
+			<br>
+			<div id="panier" class="row">
 					<tr>
-						<div class="col-sm-1"><td>ID</td></div>
-						<div class="col-sm-3"><td>Nom de l'article</td></div>
-						<div class="col-sm-6"><td>Description</td></div>
-						<div class="col-sm-2"><td>Prix</td></div>
+						<div class="col-sm-6"><td>Nombre total d'articles</td></div>
+						<div class="col-sm-6"><td>Prix total</td></div>
 					</tr>
+				
 					<tr>
-						<div class="col-sm-1"><td>1</td></div>
-						<div class="col-sm-3"><td>...</td></div>
 						<div class="col-sm-6"><td>....</td></div>
-						<div class="col-sm-2"><td>....€</td></div>
+						<div class="col-sm-6"><td>...€</td></div>
 					</tr>
-					<tr>
-						<div class="col-sm-1"><td>2</td></div>
-						<div class="col-sm-3"><td>...</td></div>
-						<div class="col-sm-6"><td>....</td></div>
-						<div class="col-sm-2"><td>....€</td></div>
-					</tr>
-					<tr>
-						<div class="col-sm-1"><td>3</td></div>
-						<div class="col-sm-3"><td>...</td></div>
-						<div class="col-sm-6"><td>....</td></div>
-						<div class="col-sm-2"><td>....€</td></div>
-					</tr>
-		</div>
-		<br>
-		<div id="panier" class="row">
-				<tr>
-					<div class="col-sm-6"><td>Nombre total d'articles</td></div>
-					<div class="col-sm-6"><td>Prix total</td></div>
-				</tr>
-			
-				<tr>
-					<div class="col-sm-6"><td>....</td></div>
-					<div class="col-sm-6"><td>...€</td></div>
-				</tr>
-		</div>
-		<br>
+			</div>
+			<br>
 				<tr><td><input type="submit" name="button" value="Passer la commande" align="right"></td></tr>
 				<br><br><br><br><br>
+	</div>
 
 </div>
 
