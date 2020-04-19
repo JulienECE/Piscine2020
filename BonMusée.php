fbf974bcef2b3789a50860b861b8d577293f0d25
JulienECE <47591531+JulienECE@users.noreply.github.com>
2020-04-19 12:53:52 +0200

    Add page produit + modif des catégories

--- BonMusée.php
+++ BonMusée.php
@@ -14,35 +14,35 @@
 	    			<td><a href="cap"><img src="cap.jpg"></a></td>
 	    			<td width="300px"><p align="center"> description</p></td>
 	    			<td width="300px"><p align="center">Prix actuel : ...€ <br>(Vente par négociation)</p></td>
-	    			<td><input type="submit" name="buttonNégo" value="Négocier"></td>
+	    			<td><input type="submit" name="buttonNégo" value="Voir plus"></td>
 	    		</tr>
 	    		<tr><td><hr></td><td><hr></td><td><hr></td><td><hr></td></tr>
 	    		<tr>
 	    			<td><a href="images/img1"><img src="images/img1.jpg"></a></td>
 	    			<td width="300px"><p align="center"> description</p></td>
 	    			<td width="300px"><p align="center">Prix actuel : ...€ <br>(Vente par enchère)</p></td>
-	    			<td><input type="submit" name="buttonEnchere" value="Enchérir"></td>
+	    			<td><input type="submit" name="buttonEnchere" value="Voir plus"></td>
 	    		</tr>
 	    		<tr><td><hr></td><td><hr></td><td><hr></td><td><hr></td></tr>
 	    		<tr>
 	    			<td><a href="images/img2"><img src="images/img2.jpg"></a></td>
 	    			<td width="300px"><p align="center"> description</p></td>
 	    			<td width="300px"><p align="center">Prix : 200€ <br>(Vente directe)</p></td>
-	    			<td><input type="submit" name="buttonAcheter" value="Acheter"></td>
+	    			<td><input type="submit" name="buttonAcheter" value="Voir plus"></td>
 	    		</tr>
 	    		<tr><td><hr></td><td><hr></td><td><hr></td><td><hr></td></tr>
 	    		<tr>
 	    			<td><a href="images/img3"><img src="images/img3.jpg"></a></td>
 	    			<td width="300px"><p align="center"> description</p></td>
 	    			<td width="300px"><p align="center">Prix : 200€ <br>(Vente directe)</p></td>
-	    			<td><input type="submit" name="buttonAcheter" value="Acheter"></td>
+	    			<td><input type="submit" name="buttonAcheter" value="Voir plus"></td>
 	    		</tr>
 	    		<tr><td><hr></td><td><hr></td><td><hr></td><td><hr></td></tr>
 	    		<tr>
 	    			<td><a href="images/img4"><img src="images/img4.jpg"></a></td>
 	    			<td width="300px"><p align="center"> description</p></td>
 	    			<td width="300px"><p align="center">Prix : 200€ <br>(Vente directe)</p></td>
-	    			<td><input type="submit" name="buttonAcheter" value="Acheter"></td>
+	    			<td><input type="submit" name="buttonAcheter" value="Voir plus"></td>
 	    		</tr>
 			</table>
 		
---
+++ produit.php
@@ -0,0 +1,20 @@
+<?php require 'header.php'; ?>
+
+<div class="container-fluid text-center">    
+  <div class="row content">
+    
+    <!-- Colonne du milieu-->
+
+    		<div class="col-sm-12">
+	    		<h4><br>Link ici le nom du produit</h4><br>
+	    		<img src="img1"><br>
+	    		<p> Link ici la description </p><br>
+	    		<p>Link ici le prix</p>
+	    		<input type="submit" name="buttonEnchere" value="Enchérir/Acheter/Offre">	
+	    	</div>
+   	</div>     
+	 
+</div>
+</div>
+
+<?php require 'footer.php'; ?>
