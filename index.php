0b31afede30530bef8d645e2e39c67fe9014170b
JulienECE <47591531+JulienECE@users.noreply.github.com>
2020-04-19 17:31:26 +0200

    tri + indentation

--- index.php
+++ index.php
@@ -3,75 +3,36 @@
 
 <!-- Colonne du milieu-->
     
-<div class="container-fluid p-0">
-      <div class="site-slider  p-0">
-
-     
-          <div class="slider-one p-0 ">
-            <div>
-             <a  href="Ferraille.php">
-              <img src="images/image4.jpg" class="img-fluid" alt="Banner 1">
-             </a>
-            </div>
-            <div>
-             <a  href="Ferraille.php">
-              <img src="images/image5.jpg" class="img-fluid" alt="Banner 2">
-             </a>
-            </div>
-            <div>
-             <a  href="Ferraille.php">
-              <img src="images/image6.jpg" class="img-fluid" alt="Banner 3">
-             </a>
-            </div>
-          </div>
-          
-        </div> 
+    <div class="container-fluid p-0">
+      <div class="site-slider  p-0">   
+        <div class="slider-one p-0 ">
+          <div><a  href="Ferraille.php"><img src="images/image4.jpg" class="img-fluid" alt="Banner 1"></a></div>
+          <div><a  href="Ferraille.php"><img src="images/image5.jpg" class="img-fluid" alt="Banner 2"></a></div>
+          <div><a  href="Ferraille.php"><img src="images/image6.jpg" class="img-fluid" alt="Banner 3"></a></div>
+        </div>
+      </div> 
     </div>
 
     <div class="container-fluid ">
       <div class="site-slider-two px-md-4">
         <div class="row slider-two text-center">
           <div class="col-md-2 product pt-md-5 pt-4">
-
-
-           <a class="nomItem" href="#">
-            
-          
-
-            <img src="./images/img1.jpg" alt="img1"/>
-            <span class="border site-btn btn-span"> 1</span>
-          </a>
-
+            <a class="nomItem" href="#"><img src="./images/img1.jpg" alt="img1"/><span class="border site-btn btn-span"> 1</span></a>
           </div>
           <div class="col-md-2 product pt-md-5 pt-4">
-            <a class="nomItem" href="#">
-            <img src="./images/img2.jpg" alt="img2"/>
-            <span class="border site-btn btn-span"> 2</span>
-            </a>
+            <a class="nomItem" href="#"><img src="./images/img2.jpg" alt="img2"/><span class="border site-btn btn-span"> 2</span></a>
           </div>
           <div class="col-md-2 product pt-md-5 pt-4">
-            <a class="nomItem" href="#">
-            <img src="./images/img3.jpg" alt="img3"/>
-            <span class="border site-btn btn-span"> 3</span>
-            </a>
+            <a class="nomItem" href="#"><img src="./images/img3.jpg" alt="img3"/><span class="border site-btn btn-span"> 3</span></a>
           </div>
           <div class="col-md-2 product pt-md-5 pt-4">
-            <a class="nomItem" href="#">
-            <img src="./images/img4.jpg" alt="img4"/>
-            <span class="border site-btn btn-span"> 4</span>
-            </a>
+            <a class="nomItem" href="#"><img src="./images/img4.jpg" alt="img4"/><span class="border site-btn btn-span"> 4</span></a>
           </div>
           <div class="col-md-2 product pt-md-5 pt-4">
-            <a class="nomItem" href="#">
-            <img src="./images/img5.jpg" alt="img5"/>
-            <span class="border site-btn btn-span"> 5</span>
-            </a>
+            <a class="nomItem" href="#"><img src="./images/img5.jpg" alt="img5"/><span class="border site-btn btn-span"> 5</span></a>
           </div>
           <div class="col-md-2 product pt-md-5 pt-4">
-            <a class="nomItem" href="#">
-            <img src="./images/img6.jpg" alt="img6"/>
-            <span class="border site-btn btn-span"> 6</span>
-            </a>
+            <a class="nomItem" href="#"><img src="./images/img6.jpg" alt="img6"/><span class="border site-btn btn-span"> 6</span></a>
           </div>
         </div>
       </div>
