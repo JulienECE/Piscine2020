fec77aeb04bc31995946068628af8cfb73126c45
JulienECE <47591531+JulienECE@users.noreply.github.com>
2020-04-19 17:40:00 +0200

    tri

--- Register.php
+++ Register.php
@@ -1,34 +1,29 @@
 <?php require 'header.php'; ?>
 
 <div class="container-fluid text-center">    
-  <div class="row content">
-  	
-    
-    <!-- Colonne du milieu-->
-
-    <div class="col-sm-12"> 
-    	<br>
-      <h3>Bienvenue parmis nous veuillez renseigner vos informations pour vous enregistrer</h3>
-      <br>
-    	<center>
-		    <form action="addlog.php" method="post" class="formulaire_val">
-		      <table><br>
-		          <tr><input type="text" placeholder="Nom" class="inputbasic firstinput" name="nom"/></tr>
-		          <br>
-			       <tr><input type="text" placeholder="Email" class="inputbasic" name="email"/></tr>
-			       <br>
-			       <tr><p>Choisir une photo de profil<p/></tr>
-			       <tr><input type="file" class="" placeholder="a"/></tr>
-			       <br>
-			       <br>
-			       <input type="submit" name="button" value="Valider"/>
-		      </table>
-		    </form>
-		    <br><br><br><br>
-		  </center>
-   	</div>     
-	 
-</div>
+	<div class="row content">
+		<div class="col-sm-12"> 
+    		<br>
+      		<h3>Bienvenue parmis nous veuillez renseigner vos informations pour vous enregistrer</h3>
+      		<br>
+    		<center>
+		   		<form action="addlog.php" method="post" class="formulaire_val">
+		      		<table><br>
+		          		<tr><input type="text" placeholder="Nom" class="inputbasic firstinput" name="nom"/></tr>
+		          		<br>
+			       		<tr><input type="text" placeholder="Email" class="inputbasic" name="email"/></tr>
+			       		<br>
+			       		<tr><p>Choisir une photo de profil<p/></tr>
+			       		<tr><input type="file" class="" placeholder="a"/></tr>
+			       		<br>
+			       		<br>
+			       		<input type="submit" name="button" value="Valider"/>
+		      		</table>
+		    	</form>
+		    	<br><br><br><br>
+		  	</center>
+   		</div>     
+	</div>
 </div>
 
 <?php require 'footer.php'; ?>
--- produit.php
+++ produit.php
@@ -1,20 +1,16 @@
 <?php require 'header.php'; ?>
 
-<div class="container-fluid text-center">    
-  <div class="row content">
-    
-    <!-- Colonne du milieu-->
-
-    		<div class="col-sm-12">
-	    		<h4><br>Link ici le nom du produit</h4><br>
-	    		<img src="img1"><br>
-	    		<p> Link ici la description </p><br>
-	    		<p>Link ici le prix</p>
-	    		<input type="submit" name="buttonEnchere" value="Enchérir/Acheter/Offre">	
-	    	</div>
-   	</div>     
-	 
-</div>
+	<div class="container-fluid text-center">    
+		<div class="row content">
+	   		<div class="col-sm-12">
+		    	<h4><br>Link ici le nom du produit</h4><br>
+		    	<img src="img1"><br>
+		    	<p> Link ici la description </p><br>
+		    	<p>Link ici le prix</p>
+		    	<input type="submit" name="buttonEnchere" value="Enchérir/Acheter/Offre">	
+		    </div>
+		</div>      
+	</div>
 </div>
 
 <?php require 'footer.php'; ?>
