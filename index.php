f01310f3d8bd1f299e8a3cc2e52c7325fdc5e0d4
JulienECE <47591531+JulienECE@users.noreply.github.com>
2020-04-20 09:45:04 +0200

    Ajout d'un texte dans l'index

--- index.php
+++ index.php
@@ -3,16 +3,14 @@
 
 <!-- Colonne du milieu-->
     
-<div class="container-fluid p-0">
-      <div class="site-slider  p-0">
-
-     
-          <div class="slider-one p-0 ">
-            <div>
-             <a  href="#">
-              <img src="images/image4.jpg" class="img-fluid" alt="Banner 1">
-             </a>
-            </div>
+  <div class="container-fluid p-0">
+    <div class="site-slider  p-0">
+      <div class="slider-one p-0 ">
+        <div>
+          <a  href="#">
+            <img src="images/image4.jpg" class="img-fluid" alt="Banner 1">
+          </a>
+          </div>
             <div>
              <a  href="#">
               <img src="images/image5.jpg" class="img-fluid" alt="Banner 2">
@@ -27,12 +25,12 @@
              <a  href="#">
               <img src="images/image7.jpg" class="img-fluid" alt="Banner 1">
              </a>
-            </div>
-          </div>
-          
-        </div> 
-
-    </div>
+        </div>
+      </div>
+    </div> 
+  </div>
+  <br>
+  <h4 align="center">Bienvenue sur ECE Ebay, achetez ou vendez des vêtements en toute simplicité </h4>
 
     <div class="container-fluid ">
       <div class="site-slider-two px-md-4">
