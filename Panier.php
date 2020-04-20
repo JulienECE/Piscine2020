0941d6af3e3c05e726567495a8764783253d5bf1
Gauthier Pichon <gauthier.pichon@edu.ece.fr>
2020-04-20 17:19:08 +0200

    BO PANIER

--- Panier.php
+++ Panier.php
@@ -4,10 +4,13 @@
 		 foreach($_GET as $name => $content) { // Most people refer to $key => $value
 		  
 		}
+
 		if(!isset($_SESSION['tableau'])){
 			$tableau = Array();
+			$prices = Array();
 			$_SESSION['tableau']=$tableau;
 		}
+
 		if(isset($name)){
 			$constante = 0;
 			foreach($_SESSION['tableau'] as $element)
@@ -16,65 +19,106 @@
 				{
 					$constante=1;
 				}
-				echo $element . '<br />'; // affichera $prenoms[0], $prenoms[1] etc.
+				 // affichera $prenoms[0], $prenoms[1] etc.
 			}
 			if($constante==0)
 			{
 				array_push($_SESSION['tableau'], $name);
 			}
+			$nb = count($_SESSION['tableau']);//longueur du tab
+			
 		}
-
+		
 
 ?>
-<div class="container-fluid p-0">
-    	<h3>Votre Panier<br><br></h3>
-    	<div class="container-fluid">
-	    	<div id="panier" class="row">
-						<tr>
-							<div class="col-sm-1"><td>ID</td></div>
-							<div class="col-sm-3"><td>Nom de l'article</td></div>
-							<div class="col-sm-6"><td>Description</td></div>
-							<div class="col-sm-2"><td>Prix</td></div>
-						</tr>
-						<tr>
-							<div class="col-sm-1"><td>1</td></div>
-							<div class="col-sm-3"><td>...</td></div>
-							<div class="col-sm-6"><td>....</td></div>
-							<div class="col-sm-2"><td>....€</td></div>
-						</tr>
-						<tr>
-							<div class="col-sm-1"><td>2</td></div>
-							<div class="col-sm-3"><td>...</td></div>
-							<div class="col-sm-6"><td>....</td></div>
-							<div class="col-sm-2"><td>....€</td></div>
-						</tr>
-						<tr>
-							<div class="col-sm-1"><td>3</td></div>
-							<div class="col-sm-3"><td>...</td></div>
-							<div class="col-sm-6"><td>....</td></div>
-							<div class="col-sm-2"><td>....€</td></div>
-						</tr>
-			</div>
-			<br>
-			<div id="panier" class="row">
-					<tr>
-						<div class="col-sm-6"><td>Nombre total d'articles</td></div>
-						<div class="col-sm-6"><td>Prix total</td></div>
-					</tr>
-				
-					<tr>
-						<div class="col-sm-6"><td>....</td></div>
-						<div class="col-sm-6"><td>...€</td></div>
-					</tr>
-			</div>
-			<br>
-				<tr><td><form action="paiement.php" method="get" class="formulaire_val">	
-				<input type="submit" name="'.$_SESSION['tableau'].'" value="Passer la commande" align="right">
-				</form></td></td></tr>
-				<br><br><br><br><br>
-				
-	</div>
 
-</div>
+<?php
+		
+		$database = "bdd";
+		$db_handle = mysqli_connect('localhost', 'root', '');
+		$db_found = mysqli_select_db($db_handle, $database);
+		
+		if ($db_found) {
+
+?>
+		<div class="container">
+			<center>
+			<h3>Votre Panier<br><br></h3>
+			<div class="table-responsive">
+				
+			<table width="100%">
+				<tr>
+					<th width="70%"> Article</th>
+					<th width="15%"> Etat</th>
+					<th width="15%"> Prix</th>
+		<?php
+
+		//tester s'il y a de résultat
+		for($i=0;$i<$nb;$i++){
+
+		
+		$sql = "SELECT * FROM item WHERE ID = '".$_SESSION['tableau'][$i]."' LIMIT 10";
+		$result = mysqli_query($db_handle, $sql);
+		while ($data = mysqli_fetch_assoc($result))
+			{
+				?>
+
+		
+					
+
+
+
+				<tr>  
+                            <td><?php echo $data['TITRE']; ?></td>  
+                            <td><?php echo $data['ETAT']; ?></td>  
+                            <td><?php echo $data['PRIX']; ?> €</td>  
+                            
+                </tr>  
+				
+			 	
+
+		<?php
+		$prices[$i] = $data['PRIX'];
+		}
+		
+		}
+		$total= array_sum($prices);
+		
+		
+		?>
+		<tr>  
+                               <td colspan="3" align="right"><h4>Total :  </h4></td>  
+                               <td align="right"><h4> <?php echo $total; ?>€ </h4></td>  
+                               
+                                
+                          </tr>  
+
+
+				</tr>
+			</table>
+			
+		</div>
+				<form action="paiement.php" method="get" class="formulaire_val">	
+				<input type="submit" name="'.$_SESSION['tableau'].'" value="Passer la commande" >
+				</form>
+			</center>
+		</div>
+		<?php
+
+
+
+		}
+		
+		else
+		{
+			header('Location: ./index.php');
+			exit();
+		}
+			?>
+
+
+
+
+
 
 <?php require 'footer.php'; ?>
