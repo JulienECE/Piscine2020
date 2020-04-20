41d7142fd292d52c9c89465e5533610c45806f21
JulienECE <47591531+JulienECE@users.noreply.github.com>
2020-04-20 10:37:33 +0200

    Page profilAcheteur.php

---
+++ profilAcheteur.php
@@ -0,0 +1,20 @@
+<?php require 'header.php'; ?>
+
+<!-- Colonne du milieu-->
+
+<div class="container-fluid">    
+	<br>
+	<table>
+		<center>
+		<tr>
+			<td><img width = "200" src="images/a2"></a></td>
+			<td width="300px" height="200px" ><p align="center">Prix :<br>Type de vente</p></td>
+			<td><form action="#" method="post" class="">	
+			<input type="submit" name="buttonSuppr" value="Supprimer cet article">
+			</form></td>
+		</tr>
+		</center>
+	</table>
+</div>
+
+<?php require 'footer.php'; ?>
