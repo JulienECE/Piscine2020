aa85b06ef3820190353531087da77d54942a6b05
Gauthier Pichon <gauthier.pichon@edu.ece.fr>
2020-04-20 11:50:04 +0200

    ==

--- profilVendeur.php
+++ profilVendeur.php
@@ -2,17 +2,49 @@
 
 <!-- Colonne du milieu-->
 
-<div class="container-fluid">    
+<div class="container">    
 	<br>
 	<table>
 		<center>
-		<tr>
-			<td><img width = "200" src="images/a2"></a></td>
-			<td width="300px" height="200px" ><p align="center">Prix :<br>Type de vente</p></td>
-			<td><form action="#" method="post" class="">	
-			<input type="submit" name="buttonSuppr" value="Supprimer cet article">
-			</form></td>
-		</tr>
+			<h4>Mes articles en vente</h4>
+		<?php
+		
+		$database = "bdd";
+		$db_handle = mysqli_connect('localhost', 'root', '');
+		$db_found = mysqli_select_db($db_handle, $database);
+		
+		if ($db_found) {
+
+		//tester s'il y a de résultat
+		
+		
+		$sql = "SELECT * FROM item WHERE NOM_VENDEUR = '".$_SESSION['username']."' LIMIT 10";
+		$result = mysqli_query($db_handle, $sql);
+		while ($data = mysqli_fetch_assoc($result))
+			{
+				
+
+		echo '
+			<tr>
+				<td><a href="'.$data['PHOTO'].'"><img width = "250" src="./images/'.$data['PHOTO'].'"></a></td>
+				<td width="300px" height="200px" ><p align="center">Prix : ' .$data['PRIX'].'€  <br>('.$data['TYPE_VENTE'].')</p></td>
+				<td><form action="#" method="post" class="formulaire_val">	
+				<input type="submit" name="'.$data['ID'].'" value="Mettre fin à la vente">
+				
+				</form></td>
+			</tr>';
+
+
+		}
+
+		}
+		
+		else
+		{
+			header('Location: ./index.php');
+			exit();
+		}
+			?>
 		</center>
 	</table>
 </div>
